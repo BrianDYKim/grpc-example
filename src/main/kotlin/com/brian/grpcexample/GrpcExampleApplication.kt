f97af95bbbc00a1c370af18a17c54eca3335a9c2
BrianDYKim <18k7102dy@naver.com>
2022-09-08 22:05:16 +0900

    Initial commit

---
+++ src/main/kotlin/com/brian/grpcexample/GrpcExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.brian.grpcexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrpcExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<GrpcExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/brian/grpcexample/GrpcExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.brian.grpcexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrpcExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
